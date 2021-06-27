e79a4197a4b38d7e38bd130e371306b91faa0bbd
Blaine Richardson <blainerrichardson@gmail.com>
2021-06-27 18:11:30 -0500

    feat: section-6: finishng all lectures for section-6

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ mod section_5;
 mod section_6;
 
 fn main() {
-  // section_2::run();
-  // section_3::run();
-  // section_4::run();
-  // section_5::run();
+  section_2::run();
+  section_3::run();
+  section_4::run();
+  section_5::run();
   section_6::run();
 }
